207c165789262983e8fed81dd8b3c652c94f2713
Brendan E Cunnie <saintbrendan@gmail.com>
2017-05-06 12:04:43 -0400

    Display speaker times and percentages
     - refactor out logic into new getSpeakerTimes function
    
    Signed-off-by: Brian Cunnie <brian.cunnie@gmail.com>

--- js/charlotte.js
+++ js/charlotte.js
@@ -5,29 +5,22 @@ meetingGuid = meetingGuid.split('=')[1];
 diarizationURL = 'https://diarizer.com/' + meetingGuid + '/diarization.txt';
 transcriptionURL = 'https://diarizer.com/' + meetingGuid + '/transcription.txt';
 
-var speakerTimes = {};
-
 jQuery.get(diarizationURL, function (data) {
     var lines = data.split(/\n/);
-    var out = "";
-    lines.forEach(function (line) {
-        if (line.length > 0) {
-            var fields = line.split(/\s+/);
-            var startTime = fields[2].split(/=/)[1];
-            var endTime = fields[3].split(/=/)[1];
-            var speakerNum = fields[4].split(/=/)[1].split(/_/)[1];
-            out = out + startTime + " " + endTime + " " + speakerNum + "<br />";
-            if (!speakerTimes[speakerNum] || !("time" in speakerTimes[speakerNum])) {
-                speakerTimes[speakerNum] = {time: endTime - startTime};
-                speakerTimes[speakerNum].time = 0
-            }
-            speakerTimes[speakerNum].time += endTime - startTime;
-        }
-    });
+    var totalTime = 0;
+    speakerTimes = getSpeakerTimes(lines);
+    console.log(speakerTimes);
     var times = "";
     for (var spkr in speakerTimes) {
+        totalTime += speakerTimes[spkr];
+    }
+    for (var spkr in speakerTimes) {
         console.log(spkr);
-        times += "time " + spkr + ": " + speakerTimes[spkr].time + "<br />";
+        // display the raw time values
+        jQuery('#speaker_time_' + spkr).html(speakerTimes[spkr]);
+        // calculate and display the percent
+        percent = speakerTimes[spkr] / totalTime;
+        jQuery('#speaker_percent_' + spkr).html(percent);
     }
 });
 
@@ -39,3 +32,22 @@ jQuery.get(transcriptionURL, function (data) {
     });
     jQuery('#transcription').html(out);
 });
+
+getSpeakerTimes = function (lines) {
+    var speakerTimes = {};
+    var out = "";
+    lines.forEach(function (line) {
+        if (line.length > 0) {
+            var fields = line.split(/\s+/);
+            var startTime = fields[2].split(/=/)[1];
+            var endTime = fields[3].split(/=/)[1];
+            var speakerNum = fields[4].split(/=/)[1].split(/_/)[1];
+            out = out + startTime + " " + endTime + " " + speakerNum + "<br />";
+            if (!speakerTimes[speakerNum]) {
+                speakerTimes[speakerNum] = 0;
+            }
+            speakerTimes[speakerNum] += endTime - startTime;
+        }
+    });
+    return speakerTimes;
+}
